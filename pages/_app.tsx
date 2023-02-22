ec09740ca2338b38b624500306e5105568840195
leandrorochaoliveira <leandrorochaoliveira@icloud.com>
2023-02-21 22:52:29 -0300

    Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Open_Sans } from "@next/font/google";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import Script from 'next/script'
 
 const openSans = Open_Sans({
   weight: ["400", "500", "600", "800"],
@@ -9,8 +10,26 @@ const openSans = Open_Sans({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={openSans.className}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=YNKDVFB2T2`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-YNKDVFB2T2');
+        `}
+      </Script>
+
+      <div className={openSans.className}>
+        <Component {...pageProps} />
+      </div>
+    </>
+
   );
 }
